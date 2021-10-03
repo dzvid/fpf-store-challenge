914c16d57ad439cc19cb13e536d5aa2d25abf66b
David <oliveiradavid.dev@gmail.com>
2021-10-02 20:50:28 -0400

    chore: Configure app theme

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -1,6 +1,14 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 
+import theme from "@/global/styles/theme";
+
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: theme.lightThemeColors,
+    },
+  },
+});
